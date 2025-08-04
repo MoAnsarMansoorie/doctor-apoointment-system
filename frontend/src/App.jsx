620ce7b5b70d9e58d858a3d58a2e84557a97ca70
MoANsarMansoorie <moansarmansoorie@gmail.com>
2025-08-04 23:05:58 +0530

    created pages and setup react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from "./pages/Doctors"
+import MyProfile from "./pages/MyProfile"
+import MyAppointments from "./pages/MyAppointments"
+import Appointment from './pages/Appointment'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment' element={<Appointment />} />
+      </Routes>
+    </div>
   )
 }
 
 export default App
+
+
+
