217ef2edf8114635e52fd032230bd1d931dd92a7
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-08-22 02:50:34 +0530

    created abot and contact page

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,37 +1,12 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
+
 
 const MyAppointments = () => {
-  const { docId } = useParams();
-  const { doctors } = useContext(AppContext)
-  console.log(docId);
-  console.log(doctors);
-  const [docInfo, setDocInfo] = useState(null);
-  console.log(docInfo)
 
-  const fetchDocInfo = async () => {
-    const docInfo = await doctors.find(doc => doc._id === docId);
-    setDocInfo(docInfo);
-    console.log(docInfo);
-  }
-
-  useEffect(() => {
-    fetchDocInfo();
-  }, [docId, doctors]);
 
   return  (
     <div>
-      {/* -------------------------Doctor details -----------------------*/}
-      <div>
-        <div>
-          <img src={docInfo.image} alt="image" className="w-16 h-16 rounded-full" />
-        </div>
 
-        <div>
-
-        </div>
-      </div>
     </div>
   );
 }
