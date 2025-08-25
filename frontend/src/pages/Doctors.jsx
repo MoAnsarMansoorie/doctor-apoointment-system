73558056bf0dcdd41e78a30c8c5335beb5eb42dc
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-08-26 02:16:04 +0530

    make responsive of UI

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -6,6 +6,7 @@ const Doctors = () => {
   const navigate = useNavigate();
   const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([])
+  const [showFilter, setShowFilter] = useState(false)
 
   const { doctors } = useContext(AppContext);
 
@@ -25,7 +26,8 @@ const Doctors = () => {
     <div>
       <p className='text-gray-600'>Browse doctors by speciality</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <div className='flex flex-col gap-2 text-gray-600'>
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? "bg-primary text-white" : ""}`} onClick={() => setShowFilter(prev => !prev)}>Filters</button>
+        <div className={`flex-col gap-2 text-gray-600 ${showFilter ? "flex" : "hidden sm:flex"}`}>
           <p onClick={() => speciality === "General Physician" ? navigate("/doctors") : navigate("/doctors/General Physician")} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "General Physician" ? "bg-indigo-400 text-black" : ""}`}>General Physician</p>
           <p onClick={() => speciality === "Gynecologist" ? navigate("/doctors") : navigate("/doctors/Gynecologist")} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Gynecologist" ? "bg-indigo-400 text-black" : ""}`}>Gynecologist</p>
           <p onClick={() => speciality === "Dermatologist" ? navigate("/doctors") : navigate("/doctors/Dermatologist")} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === "Dermatologist" ? "bg-indigo-400 text-black" : ""}`}>Dermatologist</p>
