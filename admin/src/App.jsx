7f4772e50d7c8631528cbb970dab9a62db3c291a
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2025-09-11 05:39:23 +0530

    admin panel setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
